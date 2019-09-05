fcba451623f2ffdcd8f67ed4174f89c3c2a0fb57
Jonathan Dupré <hello@jonathandupre.com>
2019-09-05 11:21:07 -0400

    make it work in a browser

--- src/widget.js
+++ src/widget.js
@@ -4,7 +4,40 @@
  * https://github.com/chjj/blessed
  */
 
-var widget = exports;
+require('./widgets/ansiimage');
+require('./widgets/bigtext');
+require('./widgets/box');
+require('./widgets/button');
+require('./widgets/checkbox');
+require('./widgets/element');
+require('./widgets/filemanager');
+require('./widgets/form');
+require('./widgets/image');
+require('./widgets/input');
+require('./widgets/layout');
+require('./widgets/line');
+require('./widgets/list');
+require('./widgets/listbar');
+require('./widgets/listtable');
+require('./widgets/loading');
+require('./widgets/log');
+require('./widgets/message');
+require('./widgets/node');
+require('./widgets/overlayimage');
+require('./widgets/progressbar');
+require('./widgets/prompt');
+require('./widgets/question');
+require('./widgets/radiobutton');
+require('./widgets/radioset');
+require('./widgets/screen');
+require('./widgets/scrollablebox');
+require('./widgets/scrollabletext');
+require('./widgets/table');
+require('./widgets/text');
+require('./widgets/textarea');
+require('./widgets/textbox');
+
+var widget = module.exports
 
 widget.classes = [
   'Node',
@@ -38,22 +71,21 @@ widget.classes = [
   'Image',
   'ANSIImage',
   'OverlayImage',
-  'Video',
   'Layout'
-];
+]
 
-widget.classes.forEach(function(name) {
-  var file = name.toLowerCase();
-  widget[name] = widget[file] = require('./widgets/' + file);
-});
+widget.classes.forEach(function (name) {
+  var file = name.toLowerCase()
+  widget[name] = widget[file] = require('./widgets/' + file)
+})
 
 widget.aliases = {
-  'ListBar': 'Listbar',
-  'PNG': 'ANSIImage'
-};
+  ListBar: 'Listbar',
+  PNG: 'ANSIImage'
+}
 
-Object.keys(widget.aliases).forEach(function(key) {
-  var name = widget.aliases[key];
-  widget[key] = widget[name];
-  widget[key.toLowerCase()] = widget[name];
-});
+Object.keys(widget.aliases).forEach(function (key) {
+  var name = widget.aliases[key]
+  widget[key] = widget[name]
+  widget[key.toLowerCase()] = widget[name]
+})
